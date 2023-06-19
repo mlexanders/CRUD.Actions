5a5a579bc57e6c0e3c03c770038e760b88b16179
Sainz <Sainz@DESKTOP-OMQD4GM>
2023-06-19 15:26:11 +0300

    add base crud controller

---
+++ CRUD.Actions/BaseCrudController.cs
@@ -0,0 +1,23 @@
+﻿using CRUD.Actions.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Actions
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public abstract class BaseCrudController<TEntity, TKey> : Controller where TEntity : Entity<TKey>
+    {
+        private readonly BaseCrudRepository<TEntity, TKey> repository;
+
+        public BaseCrudController(BaseCrudRepository<TEntity, TKey> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPost]
+        public virtual async Task Create([FromBody] TEntity entity)
+        {
+            await repository.Create(entity);
+        }
+    }
+}
